7864a007820fafa13f1b61f74ef48f9dc2533692
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-05-23 01:43:38 +0700

    Initial Commit

---
+++ lib/home_page.dart
@@ -0,0 +1,47 @@
+import 'package:bloc_tutorial/cubit/counter_cubit.dart';
+import 'package:bloc_tutorial/cubit/increment_decrement_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    final counterCubit = BlocProvider.of<CounterCubit>(context);
+
+    return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            title: Text(title),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'You have pushed the button this many times:',
+                ),
+                BlocBuilder<CounterCubit, int>(
+                    bloc: counterCubit,
+                    builder: (context, counter) {
+                      return  Text(
+                      '$counter',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    );
+                  }
+                ),
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (_) => const IncrementDecrementPage()));
+            },
+            child: const Icon(Icons.navigate_next),
+          ),
+        );
+  }
+}
